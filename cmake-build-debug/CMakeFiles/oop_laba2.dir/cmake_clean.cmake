10babf9f7c863b10a9141b8455b091fc0bb71322
Геворг Цатурян <gstsaturyan@miem.hse.ru>
2021-11-29 14:03:07 +0300

    start

---
+++ cmake-build-debug/CMakeFiles/oop_laba2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oop_laba2.dir/main.cpp.o"
+  "oop_laba2"
+  "oop_laba2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oop_laba2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
